30fadc979c3d90e9f29f0866efb33c0f0777e8a4
mikk369 <mikk.ereline@vikk.ee>
2022-06-21 00:13:58 +0300

    added calc logic

--- calc.js
+++ calc.js
@@ -1,13 +1,38 @@
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-// const tips = [];
-// const totals = [];
+//Calculate Tip
+function calculateTip() {
+  let billAmt = document.getElementsByClassName("billamt").value;
+  let billProt = document.getElementsByClassName("billprot").value;
+  let billSpl = document.getElementsByClassName("billspl").value;
 
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  // totals.push(tip + bills[i]);
+  //validate input
+  if (billAmt === "" || billProt === "") {
+    alert("Please enter values");
+    return;
+  }
+
+  //Check to see if this input is empty or less than or equal to 1
+  if (billSpl === "" || billSpl <= 1) {
+    billSpl = 1;
+    document.getElementsByClassName("each").style.display = "none";
+  } else {
+    document.getElementsByClassName("each").style.display = "block";
+  }
+
+  //Calculate tip
+  var total = (billAmt * billProt) / billSpl;
+  //round to two decimal places
+  total = Math.round(total * 100) / 100;
+  //next line allows us to always have two digits after decimal point
+  total = total.toFixed(2);
+  //Hide the tip amount on load
+  document.getElementById("totalTip").style.display = "none";
+  document.getElementById("each").style.display = "none";
+  //Display the tip
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerHTML = total;
+
+  //click to call function
+  document.getElementById("calculate").onclick = function () {
+    calculateTip();
+  };
 }
-console.log(bills, tips, totals);
