119011740ef5977edecb8562e1dd25c546fd53d0
mikk369 <mikk.ereline@vikk.ee>
2022-06-21 19:46:32 +0300

    added calculation

--- calc.js
+++ calc.js
@@ -1,38 +1,28 @@
-//Calculate Tip
-function calculateTip() {
-  let billAmt = document.getElementsByClassName("billamt").value;
-  let billProt = document.getElementsByClassName("billprot").value;
-  let billSpl = document.getElementsByClassName("billspl").value;
+calculate.addEventListener("click", () => {
+  // let calculate = document.querySelector("#calculate");
+  let billamt = document.querySelector(".billamt").value;
+  let billprot = document.querySelector("#billprot").value;
+  let billspl = document.querySelector(".billspl").value;
+  let tip = document.querySelector("tip").value;
 
-  //validate input
-  if (billAmt === "" || billProt === "") {
+  //validate
+  if (billamt === "" || billprot == 0) {
     alert("Please enter values");
     return;
   }
-
-  //Check to see if this input is empty or less than or equal to 1
-  if (billSpl === "" || billSpl <= 1) {
-    billSpl = 1;
-    document.getElementsByClassName("each").style.display = "none";
-  } else {
-    document.getElementsByClassName("each").style.display = "block";
-  }
-
-  //Calculate tip
-  var total = (billAmt * billProt) / billSpl;
-  //round to two decimal places
-  total = Math.round(total * 100) / 100;
-  //next line allows us to always have two digits after decimal point
+  //calculate
+  let total = (billamt * billprot) / billspl;
   total = total.toFixed(2);
-  //Hide the tip amount on load
-  document.getElementById("totalTip").style.display = "none";
-  document.getElementById("each").style.display = "none";
-  //Display the tip
+  //if there is more than 1 to split then show otherwise dont
+  if (billspl === "" || billspl <= 1) {
+    billspl = 1;
+    document.getElementById("each").style.display = "none";
+  } else {
+    document.getElementById("each").style.display = "block";
+  }
   document.getElementById("totalTip").style.display = "block";
   document.getElementById("tip").innerHTML = total;
 
-  //click to call function
-  document.getElementById("calculate").onclick = function () {
-    calculateTip();
-  };
-}
+  console.log(total);
+  //TODO:Hide the tip amount on load
+});
